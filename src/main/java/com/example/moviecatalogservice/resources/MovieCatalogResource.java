5be63ddd0aa7112f2e4d89a5502b0f017ec61619
Jeff Beier <jeff.beier@cygrp.com>
2021-12-23 16:08:01 -0600

    make an api call to get movies by id

--- src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,10 +1,12 @@
 package com.example.moviecatalogservice.resources;
 
 import com.example.moviecatalogservice.models.CatalogItem;
+import com.example.moviecatalogservice.models.Movie;
 import com.example.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -17,17 +19,21 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+        RestTemplate restTemplate = new RestTemplate();
+
         //get all rated movie ids
             //hard coded for initial testing
         List<Rating> ratings = Arrays.asList(
-                new Rating(1234, 4),
-                new Rating(5678,3)
+                new Rating("1234", 4),
+                new Rating("5678",3)
         );
 
         return ratings.stream().map(rating -> {
-            new CatalogItem("Transformers", "Movie description", 4)
+            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
         })
-                .collect(Collectors.toList());
+        .collect(Collectors.toList());
         //for each movie id, call movie info service and get details
 
         //put them all together
